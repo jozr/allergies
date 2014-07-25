fafb6b0bc3fd99edb1ba5dd38b69c488f2189ffb
Josephine Wright <student@epicodus.com>
2014-07-24 17:23:04 -0700

    finally working

---
+++ lib/allergies.rb
@@ -0,0 +1,16 @@
+def allergies(input)
+  allergens = {1 => "eggs", 2 => "peanuts", 4 => "shellfish", 8 => "strawberries", 16 => "tomatoes", 32 => "chocolate", 64 => "pollen", 128 => "cats"}
+
+  result = []
+
+  allergens.each do |score, all|
+    if input > 0
+      if score <= input
+        result << all
+        input = input - score
+      end
+    end
+  end
+  result
+end
+
---
+++ spec/allergies_spec.rb
@@ -0,0 +1,12 @@
+require('rspec')
+require('allergies')
+
+describe('allergies') do
+  it('returns allergies corresponding to inputted number') do
+    expect(allergies(1)).to(eq(['eggs']))
+  end
+
+  it('returns multiple allergies when a score is given') do
+    expect(allergies(3)).to(eq(['eggs', 'peanuts']))
+  end
+end
